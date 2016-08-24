d1cbb29766f711b00ac6725956b6b4a881b8829a
Natalie Thomas <Natalie Thomas>
2016-08-24 15:16:47 -0400

    Ex10 escape sequences

---
+++ ex10.rb
@@ -0,0 +1,15 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backsplash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backsplash_cat
+puts fat_cat
